afa983328d300058231d324c25145f53147ab076
untergeekDE <jan@untergeek.de>
2021-07-04 12:16:07 +0200

    Anpassung: Daten jetzt vom Github des RKI

--- scrape-helmholtz.R
+++ scrape-helmholtz.R
@@ -12,92 +12,28 @@
 #
 # jan.eggers@hr.de hr-Datenteam 
 #
-# Stand: 16.9.2020 vormittags
-
-library("rvest")
-library("tidyverse")
-library(openxlsx)
-library(readxl)
-library(googlesheets4)
-library(lubridate)
-library(openssl)
-library(httr)
-library(DatawRappr)
+# Stand: 4.7.2021
 
 # Alles weg, was noch im Speicher rumliegt
 rm(list=ls())
 
-# Init-Werte fürs Logging, das Arbeitsverzeichnis und die Keyfile-Auswahl
-server <- FALSE
 
-# ---- Logging und Update der Semaphore-Seite ----
-id_msg <- "1Q5rCvvSUn6WGcsCnKwGJ0y9PwbiML-34kyxYSYX2Qjk"
-logfile <- ""
+msgTarget <- "B10:C10"
 
-msg <- function(x,...) {
-  print(paste0(x,...))
-  # Zeitstempel in B10, Statuszeile in C10
-  d <- data.frame(b = now(tzone= "CEST"), c = paste0(x,...))
-  range_write(id_msg,d,sheet="Tabellenblatt1",
-              range="B10:C10",col_names = FALSE,reformat=FALSE)
-  if (server) Sys.sleep(5)     # Skript ein wenig runterbremsen wegen Quota
-  if (logfile != "") {
-    cat(x,...,file = logfile, append = TRUE)
-  }
-}
-
-# Argumente werden in einem String-Vektor namens args übergeben,
-# wenn ein Argument übergeben wurde, dort suchen, sonst Unterverzeichnis "src"
-
-args = commandArgs(trailingOnly = TRUE)
-if (length(args)!=0) { 
-  server <- args[1] %in% c("server","logfile")
-  if(args[1] == "logfile") logfile <- "./logs/scrape-hsm.log"
-} 
-
-sheets_email <- "corona-rki-hmsi-googlesheets@corona-rki-hmsi-2727248702.iam.gserviceaccount.com"
-sheets_filename <- "corona-rki-hmsi-2727248702-d35af1e1baab.json"
-
-# VERSION FÜR DEN SERVER 
-if (server) {
-  # Arbeitsverzeichnis, Logdatei beschreiben
-  setwd("/home/jan_eggers_hr_de/rscripts/")
-  # Authentifizierung Google-Docs umbiegen
-  sheets_keypath <- "/home/jan_eggers_hr_de/key/"
+if (file.exists("./server-msg-googlesheet-include.R")) {
+  source("./server-msg-googlesheet-include.R")
 } else {
-  # Etwas umständlich: Die Pfade zu den beiden Entwicklungs-Rechnern...
-  
-  # ...mein privater Laptop: 
-  if (dir.exists("D:/Nextcloud/hr-DDJ/projekte/covid-19")) {
-    setwd("D:/Nextcloud/hr-DDJ/projekte/covid-19")
-    sheets_keypath <- "D:/key/"
-  }  
-  
-  # ...mein Datenteam-Laptop im Sender: 
-  if (dir.exists("F:/projekte/covid-19")) {
-    setwd("F:/projekte/covid-19")
-    sheets_keypath <- "F:/creds/"
-  }  
-  
-  # Gibt es die Datei? 
-  if (!file.exists(paste0(sheets_keypath,sheets_filename))) { 
-    simpleError("Kein Keyfile!")
-  }
+  source("/home/jan_eggers_hr_de/rscripts/server-msg-googlesheet-include.R")
 }
 
 
-gs4_deauth() # Authentifizierung löschen
-gs4_auth(email=sheets_email,path=paste0(sheets_keypath,sheets_filename))
 
-msg(as.character(now()),"-- scrape-helmholtz --")
-
-# Tabelle: "Corona-Fallzahlen-Hessen"
-#id_fallzahl = "1OhMGQJXe2rbKg-kCccVNpAMc3yT2i3ubmCndf-zX0JU"
-id_fallzahl = "17s82vieTzxblhzqNmHw814F0xWN0ruJkqnFB1OpameQ"
-
+# Tabelle: Sheet "rt-helmholtz" aus dem AAA-Dokument
 # Vergleichsdaten vom Google Sheet: letztes gelesenes Datum
-fallzahl_df <- read_sheet(id_fallzahl,sheet ="rt-helmholtz")
+fallzahl_df <- read_sheet(aaa_id,sheet ="rt-helmholtz")
 lastdate <- max(as.Date(fallzahl_df$date))
+
+msg("Helmholtz-R holen - zuletzt ",lastdate)
 
 
 
@@ -127,13 +63,31 @@ while(lastdate == this_date) {
   }
 }
 
+# Berechne gleitendes 7-Tage-Mittel
+# für alle Spalten außer date
+b_df <- brics_df %>%
+  mutate(across(-date, ~ (.x +
+             lag(.x) +
+               lag(.x,n=2) +
+               lag(.x,n=3) +
+               lag(.x,n=4) +
+               lag(.x,n=5) +
+               lag(.x,n=6)) / 7))
 
-# Maximum, Minimum, Median in Spalten schreiben. Willenlos abgeschrieben. 
 
 rt_df <- brics_df %>% 
+  # Gleitendes 7-Tage-Mittel für jede Spalte bilden. 
+  mutate(across(-date, ~ (.x +
+                            lag(.x) +
+                            lag(.x,n=2) +
+                            lag(.x,n=3) +
+                            lag(.x,n=4) +
+                            lag(.x,n=5) +
+                            lag(.x,n=6)) / 7)) %>%
+# Maximum, Minimum, Median in Spalten schreiben. Willenlos abgeschrieben. 
   rowwise() %>% 
   do(as.data.frame(.) %>% { 
-    subs <- select(., 2:105)
+    subs <- select(., -date)
     mutate(., Min = as.numeric(subs) %>% min,
            Max = as.numeric(subs) %>% max,
            Med = as.numeric(subs) %>% median()) 
@@ -144,38 +98,72 @@ rt_df <- brics_df %>%
   filter(date > as.Date("2020-03-08"))
 
 msg("Schreibe Daten ins Google Sheet")
-sheet_write(rt_df,ss = id_fallzahl, sheet = "rt-helmholtz")
+sheet_write(rt_df,ss = aaa_id, sheet = "rt-helmholtz")
 
 # ---- Jetzt noch die RKI-Tabelle für R dazuholen ----
+# Seit Juli 2021 liegen die Daten nicht mehr als XLSX auf der 
+# Webseite des RKI, sondern in einem Github-Repository. 
 
-rki_r_url <- "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Projekte_RKI/Nowcasting_Zahlen.xlsx?__blob=publicationFile"
-tryCatch(rki_r_df <- read.xlsx(rki_r_url,sheet = 1,detectDates = TRUE))
-# Gerne kommen auf Blatt 1 mal die Erläuterungen. Dann mit Blatt 2 starten. 
-if (ncol(rki_r_df < 2)) {
-  tryCatch(rki_r_df <- read.xlsx(rki_r_url,sheet = 2,detectDates = TRUE))
-  
-}
+
+repo <- "robert-koch-institut/SARS-CoV-2-Nowcasting_und_-R-Schaetzung/"
+path <- "Nowcast_R_aktuell.csv"
+
+
+
+# Wann war der letzte Commit des Github-Files? Das als Prognosedatum prog_d.
+
+github_api_url <- paste0("https://api.github.com/repos/",
+                         repo,
+                         "commits?path=",path,
+                         "&page=1&per_page=1")
+github_data <- read_json(github_api_url, simplifyVector = TRUE)
+r_d <- as_date(github_data$commit$committer$date)
+msg("R-Schätzung des RKI vom ",r_d)
+
+path <- paste0("https://github.com/",
+               repo,
+               "raw/main/",
+               path)
+
+
+# Daten lesen und schauen, ob es schon eine Archivdatei dazu gibt - 
+# wenn ja, sind wir durch, 
+# wenn nein, alles aktualisieren. 
+
+#rki_r_df <- read.xlsx("daten/Nowcasting_Zahlen.xlsx", sheet=2,detectDates = TRUE)
+rki_r_df <- read_csv(path)
+
+# Der 7-Tage-R-Wert ist in Spalten 11-13: 
+# 11: Punktschätzewr
+# 12: Untergrenze
+# 13: Obergrenze
 
 rki_r_df <- rki_r_df %>%
-  select(datum_erkrankt =1, 
-         neue_punkt_og = 2, 
-         neue_lo_og = 3, 
-         neue_hi_og = 4, 
-         neue_punkt = 5, 
-         neue_lo = 6,
-         neue_hi = 7, 
-         r_punkt = 8, 
-         r_lo = 9, 
-         r_hi = 10)
+  select(datum = Datum, 
+         neue_punkt_og = PS_COVID_Faelle, 
+         neue_lo_og = UG_PI_COVID_Faelle, 
+         neue_hi_og = OG_PI_COVID_Faelle, 
+         neue_punkt = PS_COVID_Faelle_ma4, 
+         neue_lo = UG_PI_COVID_Faelle_ma4,
+         neue_hi = OG_PI_COVID_Faelle_ma4, 
+         r4t_punkt = PS_4_Tage_R_Wert, 
+         r4t_lo = UG_PI_4_Tage_R_Wert, 
+         r4t_hi = OG_PI_4_Tage_R_Wert,
+         r7t_punkt = PS_7_Tage_R_Wert,
+         r7t_lo = UG_PI_7_Tage_R_Wert,
+         r7t_hi = OG_PI_7_Tage_R_Wert) %>%
+  # alle numerisch
+  mutate(datum = as_date(datum))
 
 # RKI-Nowcast-Sheet auf Sheet pushen
-msg("Schreibe Kopie der RKI-Daten")
-sheet_write(rki_r_df,ss = id_fallzahl, sheet = "rt-rki")
+# msg("Schreibe Kopie der RKI-Daten")
+# sheet_write(rki_r_df,ss = aaa_id, sheet = "rt-rki")
+# Archivkopie nicht nötig; hebt das RKI selbst in seinem Repository auf. 
 
 r_df <- rki_r_df %>%
-  select(datum = datum_erkrankt, r_lo, r_hi) %>%
+  select(datum, r7t_lo, r7t_hi) %>%
   full_join(rt_df, by = c("datum" = "date")) %>%
-  select(datum,r_rki_lo = r_lo, r_rki_hi = r_hi,
+  select(datum,r_rki_lo = r7t_lo, r_rki_hi = r7t_hi,
          r_helmholtz_min = Min, 
          r_helmholtz_med = Med,
          r_helmholtz_max = Max)
@@ -185,7 +173,7 @@ r_df <- r_df[nrow(r_df)-(27:0),]
 
 
 msg("Schreibe Arbeitskopie r_rki_helmholtz")
-sheet_write(r_df, ss = id_fallzahl, sheet = "r_rki_helmholtz")
+sheet_write(r_df, ss = aaa_id, sheet = "r_rki_helmholtz")
 
 # ---- Pinge Datawrapper-Grafik R-Werte, wenn neue Zahlen ----
 msg("Pinge Datawrapper-Grafik")
@@ -196,4 +184,3 @@ if (this_date > lastdate) {
 } else {
   msg("OK (nur Ping)")
 }
-
